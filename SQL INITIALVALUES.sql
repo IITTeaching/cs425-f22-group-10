e4622c02ae508e8608f4c9b90bac95c6dd2694cc
ykamran16 <115177686+ykamran16@users.noreply.github.com>
2022-12-07 15:43:53 -0600

    Add files via upload

---
+++ SQL INITIALVALUES.sql
@@ -0,0 +1,18 @@
+INSERT INTO employee(namaz,position1,address,SSN,salary,branchname) VALUES ( James,
+manager, 38th St W Los Angeles, 7000, LA_branch)
+INSERT INTO employee(namaz,position1,address,SSN,salary,branchname) VALUES ( John,
+teller, 37 St E Chicago, 5000, CA_branch)
+INSERT INTO employee(namez,position1,address,SSN,salary,branchname) VALUES ( Robert,
+loan specialist, 36th St E New York, 4000, NY_branch)
+INSERT INTO branch(branch_name,address)VALUES ( LA_branch, 22nd Street W Los Angeles)
+INSERT INTO branch(branch_name, address)VALUES (CA_branch 23rd Street E Chicago)
+INSERT INTO branch(branch_name,address)VALUES ( NY_branch, 21st Street W New York)
+INSERT INTO customer (customerID,name1,addresses,homebranch) VALUES (1, Peter, 24th
+Street N Chicago, CA_branch)
+INSERT INTO customer (customerID,name1,addresses,homebranch) VALUES (2, Noah, 23rd
+Street W Los Angeles, LA_branch)
+INSERT INTO customer(customerID,name1,addresses,homebranch) VALUES (3, Elijah, 24th
+Street N New York, NY_branch)
+INSERT INTO account( customerID, accountNumber,currentBalance) VALUES ( 1, 123, 1000)
+INSERT INTO account( customerID, accountNumber,currentBalance) VALUES ( 2, 321, 5000)
+INSERT INTO account( customerID, accountNumber,currentBalance) VALUES ( 3, 333, 10000)
